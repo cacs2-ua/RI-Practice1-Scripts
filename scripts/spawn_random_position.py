0c634ff98f31465a2a33a307f1dcaf2652cbe961
cacs2 <cacs2@alu.ua.es>
2026-05-10 20:16:16 +0200

    esquivando el obstaculo 1.0

--- scripts/spawn_random_position.py
+++ scripts/spawn_random_position.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     rospy.wait_for_service("/gazebo/spawn_sdf_model")
     rospy.wait_for_service("/gazebo/spawn_urdf_model")
 
+
     #Spawn object
     spawn_model = rospy.ServiceProxy("/gazebo/spawn_sdf_model", SpawnModel)
 
